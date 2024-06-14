59ff47e864a8d5fdf7ce588bff8a380429bb6cb9
Kishorkumar28 <kishor28092002@gmail.com>
2024-06-14 17:38:46 +0530

    2

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose'
 const dbURI = 'mongodb+srv://kishor28092002:Hikishor28@airbnb-clone.rvwggib.mongodb.net/airbnb'
 
 const connectDb = async ()=>{
-
-    if(mongoose.connection.readyState===1) return;
     await mongoose.connect(dbURI);
+    // if(mongoose.connection.readyState===1) return;
+    
     // console.log(mongoose.connection.readyState)
 }
 
